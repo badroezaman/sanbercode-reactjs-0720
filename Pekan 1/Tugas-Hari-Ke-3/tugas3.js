01f6ea4d8253d6f3081b220a44fa8e9b53ff466d
badroezaman <mail.badroe@gmail.com>
2020-07-22 18:53:00 +0700

    Tugas Pekan 1 Hari ke-3

---
+++ Pekan 1/Tugas-Hari-Ke-3/tugas3.js
@@ -0,0 +1,151 @@
+// Soal 1
+// buatlah variabel-variabel seperti di bawah ini
+
+var kataPertama = "saya";
+var kataKedua = "senang";
+var kataKetiga = "belajar";
+var kataKeempat = "javascript";
+
+// gabungkan variabel-variabel di atas agar menghasilkan output
+// "saya Senang belajar JAVASCRIPT"
+
+// Jawaban Soal 1
+console.log("JAWABAN SOAL 1");
+console.log(
+  kataPertama.concat(" " + kataKedua + " " + kataKetiga + " " + kataKeempat)
+);
+
+var kalimat =
+  kataPertama + " " + kataKedua + " " + kataKetiga + " " + kataKeempat;
+console.log(kalimat);
+
+// Soal 2
+// buatlah variabel-variabel seperti di bawah ini
+
+var kataPertama = "1";
+var kataKedua = "2";
+var kataKetiga = "4";
+var kataKeempat = "5";
+
+// ubah lah variabel di atas ke dalam integer dan lakukan jumlahkan semua variabel dan tampilkan dalam output
+
+// Jawaban Soal 2
+var strInt1 = parseInt(kataPertama);
+var strInt2 = parseInt(kataKedua);
+var strInt3 = parseInt(kataKetiga);
+var strInt4 = parseInt(kataKeempat);
+var jumlah = strInt1 + strInt2 + strInt3 + strInt4;
+console.log("JAWABAN SOAL 2");
+console.log(jumlah);
+
+// Soal 3
+// buatlah variabel-variabel seperti di bawah ini
+
+// var kalimat = 'wah javascript itu keren sekali';
+
+// var kataPertama = kalimat.substring(0, 3);
+// var kataKedua; // do your own!
+// var kataKetiga; // do your own!
+// var kataKeempat; // do your own!
+// var kataKelima; // do your own!
+
+// console.log('Kata Pertama: ' + kataPertama);
+// console.log('Kata Kedua: ' + kataKedua);
+// console.log('Kata Ketiga: ' + kataKetiga);
+// console.log('Kata Keempat: ' + kataKeempat);
+// console.log('Kata Kelima: ' + kataKelima);
+
+// selesaikan variabel yang belum diisi dan hasilkan output seperti berikut:
+// Kata Pertama: wah
+// Kata Kedua: javascript
+// Kata Ketiga: itu
+// Kata Keempat: keren
+// Kata Kelima: sekali
+
+// Jawaban Soal 3
+var kalimat = "wah javascript itu keren sekali";
+
+var kataPertama = kalimat.substring(0, 3);
+var kataKedua = kalimat.substring(4, 14); // do your own!
+var kataKetiga = kalimat.substring(15, 18); // do your own!
+var kataKeempat = kalimat.substring(19, 24); // do your own!
+var kataKelima = kalimat.substring(25, 31); // do your own!
+
+console.log("JAWABAN SOAL 3");
+console.log("Kata Pertama: " + kataPertama);
+console.log("Kata Kedua: " + kataKedua);
+console.log("Kata Ketiga: " + kataKetiga);
+console.log("Kata Keempat: " + kataKeempat);
+console.log("Kata Kelima: " + kataKelima);
+
+// Soal 4
+// buatlah variabel seperti di bawah ini
+
+var nilai;
+
+// pilih angka dari 0 sampai 100, misal 75. lalu isi variabel tersebut dengan angka tersebut. lalu buat lah pengkondisian dengan if-elseif dengan kondisi
+
+// nilai >= 80 indeksnya A
+// nilai >= 70 dan nilai < 80 indeksnya B
+// nilai >= 60 dan nilai < 70 indeksnya c
+// nilai >= 50 dan nilai < 60 indeksnya D
+// nilai < 50 indeksnya E
+
+// Jawaban Soal 4
+var nilai = 88;
+
+console.log("JAWABAN SOAL 4");
+if (nilai >= 80) {
+  console.log("A");
+} else if (nilai >= 70 && nilai < 80) {
+  console.log("B");
+} else if (nilai >= 60 && nilai < 70) {
+  console.log("C");
+} else if (nilai >= 50 && nilai < 60) {
+  console.log("D");
+} else {
+  console.log("E");
+}
+
+// Soal 5
+// buatlah variabel seperti di bawah ini
+
+var tanggal = 5;
+var bulan = 1;
+var tahun = 1991;
+
+// ganti tanggal ,bulan, dan tahun sesuai dengan tanggal lahir anda dan buatlah switch case pada bulan, lalu muncul kan string nya dengan output seperti ini 22 Juli 2020 (isi di sesuaikan dengan tanggal lahir masing-masing)
+
+// Jawaban Soal 5
+
+console.log("JAWABAN SOAL 5");
+var namaBulan;
+switch (bulan) {
+  case 1: {
+    namaBulan = "Januari";
+    break;
+  }
+  case 2: {
+    namaBulan = "Februari";
+    break;
+  }
+  case 3: {
+    namaBulan = "Maret";
+    break;
+  }
+  case 4: {
+    namaBulan = "April";
+    break;
+  }
+  case 5: {
+    namaBulan = "Mei";
+    break;
+  }
+  default: {
+    console.log("tidak dilahirkan");
+    break;
+  }
+}
+
+var namaBulan = namaBulan.toUpperCase();
+console.log(tanggal + " " + namaBulan + " " + tahun);
