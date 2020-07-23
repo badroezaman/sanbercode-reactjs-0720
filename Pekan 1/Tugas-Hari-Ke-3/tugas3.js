cdd6b576f4de281d43c1004c61dd8c4e8bb0760a
badroezaman <mail.badroe@gmail.com>
2020-07-23 13:26:19 +0700

    Update Tugas-Hari-Ke-3

--- Pekan 1/Tugas-Hari-Ke-3/tugas3.js
+++ Pekan 1/Tugas-Hari-Ke-3/tugas3.js
@@ -141,6 +141,34 @@ switch (bulan) {
     namaBulan = "Mei";
     break;
   }
+  case 6: {
+    namaBulan = "Juni";
+    break;
+  }
+  case 7: {
+    namaBulan = "Juli";
+    break;
+  }
+  case 8: {
+    namaBulan = "Agustus";
+    break;
+  }
+  case 9: {
+    namaBulan = "September";
+    break;
+  }
+  case 10: {
+    namaBulan = "Oktober";
+    break;
+  }
+  case 11: {
+    namaBulan = "November";
+    break;
+  }
+  case 12: {
+    namaBulan = "Desember";
+    break;
+  }
   default: {
     console.log("tidak dilahirkan");
     break;
