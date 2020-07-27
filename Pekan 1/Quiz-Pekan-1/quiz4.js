4a1f46795156dc3b75dee13a05de2f13285fa9a9
badroezaman <mail.badroe@gmail.com>
2020-07-28 00:11:37 +0700

    revisi quiz

--- Pekan 1/Quiz-Pekan-1/quiz4.js
+++ Pekan 1/Quiz-Pekan-1/quiz4.js
@@ -1,24 +1,11 @@
 console.log("--- Tampilkan Bintang ---");
 //ketentuan: nama functionnya adalah tampilkanBintang(jumlahBintang)
 
-// var j = 7;
-// for (y = 1; y <= j; y++) {
-//   var bintang = "";
-//   for (z = j; z >= y; z--) {
-//     bintang += "*";
-//   }
-//   console.log(bintang);
-// }
-
-function tampilkanBintang(jumlahBintang) {
-  var jumlahBintang = "";
-  for (var y = 1; y <= 7; y++) {
-    for (var z = 7; z >= y; z--) {
-      jumlahBintang += "*";
-    }
-    // console.log(jumlahBintang);
-    return jumlahBintang;
+var j = 7;
+for (y = 1; y <= j; y++) {
+  var bintang = "";
+  for (z = j; z >= y; z--) {
+    bintang += "*";
   }
+  console.log(bintang);
 }
-console.log(jumlahBintang);
-// tampilkanBintang(jumlahBintang);
