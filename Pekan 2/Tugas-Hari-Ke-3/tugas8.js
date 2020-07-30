746b5fba6b268b37873b3024bdf59565740e2f75
badroezaman <mail.badroe@gmail.com>
2020-07-31 01:03:29 +0700

    Update Tugas Pekan 2 Hari Ke-3 Soal 3

--- Pekan 2/Tugas-Hari-Ke-3/tugas8.js
+++ Pekan 2/Tugas-Hari-Ke-3/tugas8.js
@@ -43,11 +43,11 @@ const kata3 = "seorang";
 const kata4 = "frontend";
 const kata5 = "developer";
 
-let kalimat = (kata1, kata2) => {
+let kalimat = () => {
   return `${kata1} ${kata2} ${kata3} ${kata4} ${kata5}`;
 };
 
-gabungan = kalimat(kata1, kata2);
+gabungan = kalimat();
 console.log(gabungan);
 
 console.log();
@@ -55,47 +55,22 @@ console.log();
 console.log("-- Soal 3 --");
 // buatlah class Book yang didalamnya terdapat property name, totalPage, price. lalu buat class baru komik yang extends terhadap buku dan mempunyai property sendiri yaitu isColorful yang isinya true atau false
 
-const buku = {
-  name: "Bumi Manusia",
-  totalPage: 535,
-  price: 100000,
-};
-
 class Book {
   constructor(name, totalPage, price) {
     this.name = name;
     this.totalPage = totalPage;
     this.price = price;
   }
-  get name1() {
-    return this.name;
-  }
-  get totalPage1() {
-    return this.totalPage;
-  }
-  get price1() {
-    return this.price;
-  }
-  present() {
-    return this.name;
-  }
 }
 class Komik extends Book {
-  constructor(name) {
-    super(name);
-    this.isColorful = true;
-  }
-  show() {
-    return (
-      this.present() +
-      " " +
-      this.totalPage +
-      " " +
-      this.price +
-      " " +
-      this.isColorful
-    );
+  constructor(name, totalPage, price, isColorful) {
+    super(name, totalPage, price);
+    this.isColorful = isColorful;
   }
 }
-buku2 = new Komik("komik", 10, 10000);
-console.log(buku2.show());
+
+let buku = new Komik("Bumi Manusia", 535, 100000);
+let dragonBall = new Komik("Dragon Ball", 20, 20000, true);
+let conan = new Komik("Conan", 30, 30000, false);
+
+console.log(buku, dragonBall, conan);
