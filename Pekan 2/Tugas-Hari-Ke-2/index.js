fc38ab1eef5f1b45d0e409bad6c8a9cbd519db29
badroezaman <mail.badroe@gmail.com>
2020-08-01 19:52:30 +0700

    Update Tugas Pekan 2 Hari Ke-2

--- Pekan 2/Tugas-Hari-Ke-2/index.js
+++ Pekan 2/Tugas-Hari-Ke-2/index.js
@@ -5,7 +5,6 @@ var books = [
   { name: "LOTR", timeSpent: 3000 },
   { name: "Fidas", timeSpent: 2000 },
   { name: "Kalkulus", timeSpent: 4000 },
-  { name: "Komik", timeSpent: 1000 },
 ];
 
 // lanjutkan code pada index.js untuk memanggil function readBooks. Buku yang akan dihabiskan adalah buku-buku di dalam array books. Pertama function readBooks menerima input waktu yang dimiliki yaitu 10000 ms (10 detik) dan books pada indeks ke-0. Setelah mendapatkan callback sisa waktu yang dikirim lewat callback, sisa waktu tersebut dipakai untuk membaca buku pada indeks ke-1. Begitu seterusnya sampai waktu habis atau semua buku sudah terbaca. Untuk melihat output, jalankan file index.js dengan node js :
@@ -14,36 +13,31 @@ var books = [
 
 // Tulis code untuk memanggil function readBooks di sini
 
-// readBooks(4000, books[0], function (sisaWaktu) {
+// var waktuTersedia = 10000;
+
+// readBooks(waktuTersedia, books[0], function (sisaWaktu) {
 //   console.log(sisaWaktu);
 //   readBooks(sisaWaktu, books[1], function (sisaWaktu) {
 //     console.log(sisaWaktu);
 //     readBooks(sisaWaktu, books[2], function (sisaWaktu) {
 //       console.log(sisaWaktu);
-//       readBooks(sisaWaktu, books[3], function (sisaWaktu) {});
-//       // console.log(check4);
+//       return sisaWaktu;
 //     });
 //   });
 // });
 
-function recursiveRead(waktu, i) {
-  readBooks(waktu, books[i], function (sisaWaktu) {
-    // console.log(sisaWaktu);
-    if (waktu >= sisaWaktu) {
-      // console.log(sisaWaktu);
-      readBooks(sisaWaktu, books[1], function (sisaWaktu) {
-        // console.log(sisaWaktu);
-        readBooks(sisaWaktu, books[2], function (sisaWaktu) {
-          // console.log(sisaWaktu);
-          readBooks(sisaWaktu, books[3], function () {});
-          // console.log(sisaWaktu);
-          return;
-        });
-      });
-    } else {
-      return;
+// Jawaban
+var booksQueue = books.length;
+var time = 10000;
+
+function execute(time, ind, booksQueue) {
+  readBooks(time, books[ind], function (remainingTime) {
+    time = remainingTime;
+    booksQueue = booksQueue - 1;
+    if (booksQueue > 0) {
+      execute(time, ind + 1, booksQueue);
     }
   });
 }
 
-recursiveRead(10000, 0);
+execute(time, 0, booksQueue);
--- Pekan 2/Tugas-Hari-Ke-2/index2.js
+++ Pekan 2/Tugas-Hari-Ke-2/index2.js
@@ -10,16 +10,34 @@ var books = [
 
 // Lanjutkan code untuk menjalankan function readBooksPromise
 
-function readAgain(waktu, i) {
-  readBooksPromise(waktu, books[i])
-    .then(function (baca) {
-      readBooksPromise(baca, books[1]).then(function (baca) {
-        readBooksPromise(baca, books[2]).then();
-      });
+// function readAgain(waktu, i) {
+//   readBooksPromise(waktu, books[i])
+//     .then(function (baca) {
+//       readBooksPromise(baca, books[1]).then(function (baca) {
+//         readBooksPromise(baca, books[2]).then();
+//       });
+//     })
+//     .catch(function (sisaWaktu) {
+//       console.log(sisaWaktu);
+//     });
+// }
+
+// readAgain(10000, 0);
+
+// Jawaban
+var booksQueue = books.length;
+var time = 10000;
+
+function execute(time, ind, booksQueue) {
+  readBooksPromise(time, books[ind])
+    .then(function (remainingTime) {
+      time = remainingTime;
+      booksQueue = booksQueue - 1;
+      if (booksQueue > 0) {
+        execute(time, ind + 1, booksQueue);
+      }
     })
-    .catch(function (sisaWaktu) {
-      console.log(sisaWaktu);
-    });
+    .catch(function (error) {});
 }
 
-readAgain(10000, 0);
+execute(time, 0, booksQueue);
